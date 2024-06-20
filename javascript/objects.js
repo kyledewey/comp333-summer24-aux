cfdac43dabf7af5f2c077751c95e7aaa14e8c0d8
Kyle Dewey <kyle.dewey@csun.edu>
2024-06-20 15:05:01 -0700

    code from class

--- javascript/objects.js
+++ javascript/objects.js
@@ -12,19 +12,30 @@
 // let rectanglePrototype = {};
 // rectanglePrototype.getArea = getArea;
 
-let rectanglePrototype = {
-    'getArea': function () {
-	return this.width * this.height;
-    }
-}
+// let rectanglePrototype = {
+//     'getArea': function () {
+// 	return this.width * this.height;
+//     }
+// }
 
 function Rectangle(width, height) {
     // this = {};
+    // this.__proto__ = Rectangle.prototype;
     this.width = width;
     this.height = height;
-    this.__proto__ = rectanglePrototype;
+    // this.__proto__ = rectanglePrototype;
     // this.getArea = function () {
     // 	return this.width * this.height;
     // };
     // return this;
+}
+Rectangle.prototype.getArea = function() {
+    return this.width * this.height;
+}
+Rectangle.prototype.getWidth = function () {
+    return this.width;
+}
+
+function add(x, y) {
+    return x + y;
 }
